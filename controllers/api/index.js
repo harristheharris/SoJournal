a2c42ba5698d30dfd8e86b2ec51ae5442e834fe3
harristheharris <anthonyharris7777@gmail.com>
2024-04-07 18:20:04 -0400

    began the api routes

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,6 +1,6 @@
 const router = require('express').Router();
 const userRoutes = require('./userRoutes');
-const eventRoutes = require('./eventRoutes');
+const eventRoutes = require('./eventRoutes.js');
 const tripRoutes = require('./tripRoutes');
 
 
