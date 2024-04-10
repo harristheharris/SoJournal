f1b8007b336dee5b779563a222253686b7273f12
harristheharris <anthonyharris7777@gmail.com>
2024-04-10 00:00:51 -0400

    one route left

--- controllers/api/tripRoutes.js
+++ controllers/api/tripRoutes.js
@@ -5,10 +5,34 @@ const withAuth = require('../../utils/auth')
 router.post('/', withAuth, async (req, res ) => {
     try{
         const newTrip = await Trip.create({
-            
-        })
-    } catch {
+            ...req.body,
+            user_id: req.session.user_id,
 
+        });
+        res.status(200).json(newTrip)
+    } catch (err) {
+        res.status(400).json(err);
+    }
+})
+
+router.delete('/:id', withAuth, async (req, res) => {
+    try {
+        const tripData = await Trip.destroy({
+            where: {
+                id: req.params.id,
+                user_id: req.session.user_id,
+            }
+        });
+
+        if (!tripData) {
+            res.status(404).json({ message: 'No project found with this id '});
+            return;
+        }
+
+        res.status(200).json(tripData);
+
+    } catch (err) {
+        res.status(500).json(err);
     }
 })
 
