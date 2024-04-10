4dcdb6be9acf777332d5be1f018446da1223f85d
Stephen Bates <stephen.michael.bates@outlook.com>
2024-04-10 12:56:29 -0400

    fixed login route

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -21,7 +21,7 @@ router.post('/', async (req, res) => {
 
 router.post('/login', async (req, res) => {
     try {
-        const userData = await User.findOne({ where: { user_name: req.body.username } });
+        const userData = await User.findOne({ where: { username: req.body.username } });
 
         if (!userData) {
             res.status(400).json({ message: 'Incorrect email or password. Input correct email and password'})
