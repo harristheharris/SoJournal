8226be8573888bfe4e4480b495e4c90bd8f78276
Stephen Bates <stephen.michael.bates@outlook.com>
2024-04-10 06:37:34 -0400

    added handlebars

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const session = require('express-session');
+const exphbs = require('express-handlebars');
 const routes = require('./controllers');
+const helpers = require('./utils/helpers');
+const path = require('path');
 
 const sequelize = require('./config/connection');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
@@ -9,9 +12,10 @@ const app = express();
 const PORT = process.env.PORT || 3001;
 
 const sess = {
+    secret: process.env.SESS_SECRET,
     cookie: {},
     resave: false,
-    saveUninitialized: true,
+    saveUninitialized: false,
     store: new SequelizeStore({
         db: sequelize
     })
@@ -19,8 +23,12 @@ const sess = {
 
 app.use(session(sess));
 
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(routes);
 
